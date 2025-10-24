3928c755c0f34e8e9a3ba33187c5e36fc8ab1119
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2025-10-24 11:40:28 -0700

    Polynomial features

--- polynomial.py
+++ polynomial.py
@@ -3,4 +3,78 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder,OrdinalEncoder,OneHotEncoder,PolynomialFeatures
 from sklearn.linear_model import LogisticRegression
-fro
+from sklearn.metrics import precision_score,recall_score,f1_score,confusion_matrix
+from sklearn.model_selection import train_test_split
+
+df=pd.read_csv("airpollution.csv")
+
+df["Country"].fillna(df["Country"].mode()[0],inplace=True)
+df["City"].fillna(df["City"].mode(),inplace=True)
+
+print(df.isnull().sum())   
+
+print(df.head())
+
+# sns.boxplot(data=df)
+# plt.show()
+print(df.info())
+print(df.shape)
+
+q1=df["PM2.5 AQI Value"].quantile(0.25)
+q3=df["PM2.5 AQI Value"].quantile(0.75)
+
+iqr=q3-q1
+min=q1-(1.5*iqr)
+max=q3+(1.5*iqr)
+filtered_data = df[(df["PM2.5 AQI Value"] >= min) & (df["PM2.5 AQI Value"] <= max)]
+print(filtered_data.shape)
+
+print(filtered_data.head(10))
+print(filtered_data.columns)
+
+filtered_data = filtered_data.dropna(subset=[
+    "AQI Value","CO AQI Value","Ozone AQI Value","NO2 AQI Value","PM2.5 AQI Value",
+    "Country","City",
+    "AQI Category","CO AQI Category","Ozone AQI Category","NO2 AQI Category",
+    "PM2.5 AQI Category"
+]).reset_index(drop=True)
+
+x=filtered_data[["AQI Value","CO AQI Value","Ozone AQI Value","NO2 AQI Value","PM2.5 AQI Value"]]
+le=LabelEncoder()
+y=le.fit_transform(filtered_data["PM2.5 AQI Category"])
+
+oe=OrdinalEncoder()
+feature=filtered_data[["Country","City"]]
+encode_data=oe.fit_transform(feature)
+col=oe.get_feature_names_out(feature.columns)
+encode_dataframe=pd.DataFrame(encode_data,columns=col)
+
+X=pd.concat([x,encode_dataframe],axis=1)
+
+fe1=filtered_data[["AQI Category","CO AQI Category","Ozone AQI Category","NO2 AQI Category"]]
+oh=OneHotEncoder(sparse_output=False,drop="first")
+encode=oh.fit_transform(fe1)
+col1=oh.get_feature_names_out(fe1.columns)
+data=pd.DataFrame(encode,columns=col1)
+
+X_final=pd.concat([X,data],axis=1)
+
+pf=PolynomialFeatures(degree=1)
+X_p=pf.fit_transform(X_final)
+
+x_train,x_test,y_train,y_test=train_test_split(X_p,y,test_size=0.2,random_state=42)
+
+lr=LogisticRegression()
+lr.fit(x_train,y_train)
+
+print(lr.score(x_train,y_train))
+
+print(lr.score(x_test,y_test))
+
+print(precision_score(y_test,lr.predict(x_test),average="macro"))
+print(recall_score(y_test,lr.predict(x_test),average="macro"))
+print(f1_score(y_test,lr.predict(x_test),average="macro"))
+
+s=confusion_matrix(y_test,lr.predict(x_test))
+sns.heatmap(data=s,annot=True)
+plt.show()
