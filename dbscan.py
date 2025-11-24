7ebb8b9f6c28dae266c7eaf7d3a6ca7b38e3321d
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2025-11-24 09:47:42 -0800

    DBSCAN

---
+++ dbscan.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder,OrdinalEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score
+
+df=pd.read_csv("airpollution.csv")
+
+print(df.columns)
+
+df["Country"].fillna(df["Country"].mode()[0],inplace=True)
+df["City"].fillna(df["City"].mode(),inplace=True)
+
+print(df.isnull().sum())   
+
+print(df.head())
+
+# sns.boxplot(data=df)
+# plt.show()
+print(df.info())
+print(df.shape)
+
+q1=df["PM2.5 AQI Value"].quantile(0.25)
+q3=df["PM2.5 AQI Value"].quantile(0.75)
+
+iqr=q3-q1
+min=q1-(1.5*iqr)
+max=q3+(1.5*iqr)
+filtered_data = df[(df["PM2.5 AQI Value"] >= min) & (df["PM2.5 AQI Value"] <= max)]
+print(filtered_data.shape)
+
+print(filtered_data.head(10))
+print(filtered_data.columns)
+
+filtered_data = filtered_data.dropna(subset=[
+    "AQI Value","CO AQI Value","Ozone AQI Value","NO2 AQI Value","PM2.5 AQI Value",
+    "Country","City",
+    "AQI Category","CO AQI Category","Ozone AQI Category","NO2 AQI Category",
+    "PM2.5 AQI Category"
+]).reset_index(drop=True)
+
+x=filtered_data[["AQI Value","CO AQI Value","Ozone AQI Value","NO2 AQI Value","PM2.5 AQI Value"]]
+le=LabelEncoder()
+y=le.fit_transform(filtered_data["PM2.5 AQI Category"])
+
+oe=OrdinalEncoder()
+feature=filtered_data[["Country","City"]]
+encode_data=oe.fit_transform(feature)
+col=oe.get_feature_names_out(feature.columns)
+encode_dataframe=pd.DataFrame(encode_data,columns=col)
+
+X=pd.concat([x,encode_dataframe],axis=1)
+
+fe1=filtered_data[["AQI Category","CO AQI Category","Ozone AQI Category","NO2 AQI Category"]]
+oh=OneHotEncoder(sparse_output=False,drop="first")
+encode=oh.fit_transform(fe1)
+col1=oh.get_feature_names_out(fe1.columns)
+data=pd.DataFrame(encode,columns=col1)
+
+X_final=pd.concat([X,data],axis=1)
+
+db=DBSCAN(eps=100,min_samples=100)
+
+db.fit_predict(X_final)
+
+xm=db.labels_
+print(silhouette_score(X_final,xm))
